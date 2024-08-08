485e00cbc930e58956c8787659a3c45f62e761d4
MsKavitha12 <143311860+MsKavitha12@users.noreply.github.com>
2024-08-08 18:32:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
-function rollDice() {
-    var randomnumber1 = Math.floor(Math.random() * 6) + 1;
-    var randomdiceimage1 = "dice" + randomnumber1 + ".png";
+// Variables to store the results of each dice roll
+var roll1 = null;
+var roll2 = null;
+
+// Function to roll the first dice
+function rollFirstDice() {
+    roll1 = Math.floor(Math.random() * 6) + 1;
+    var randomdiceimage1 = "dice" + roll1 + ".png";
     var randomimagesource1 = "images/" + randomdiceimage1;
     var img1 = document.querySelectorAll("img")[0];
     img1.setAttribute("src", randomimagesource1);
 
-    var randomnumber2 = Math.floor(Math.random() * 6) + 1;
-    var randomdiceimage2 = "dice" + randomnumber2 + ".png";
+    checkResult();
+}
+
+// Function to roll the second dice
+function rollSecondDice() {
+    roll2 = Math.floor(Math.random() * 6) + 1;
+    var randomdiceimage2 = "dice" + roll2 + ".png";
     var randomimagesource2 = "images/" + randomdiceimage2;
     var img2 = document.querySelectorAll("img")[1];
     img2.setAttribute("src", randomimagesource2);
 
-    if (randomnumber1 === randomnumber2) {
-        document.querySelector("h1").innerHTML = "Draw";
-    } else if (randomnumber1 > randomnumber2) {
-        document.querySelector("h1").innerHTML = "Player 1 wins!";
-    } else {
-        document.querySelector("h1").innerHTML = "Player 2 wins!";
+    checkResult();
+}
+
+// Function to check if both dice have been rolled and display the result
+function checkResult() {
+    if (roll1 !== null && roll2 !== null) {
+        if (roll1 === roll2) {
+            document.querySelector("h1").innerHTML = "Draw";
+        } else if (roll1 > roll2) {
+            document.querySelector("h1").innerHTML = "Player 1 wins!";
+        } else {
+            document.querySelector("h1").innerHTML = "Player 2 wins!";
+        }
+
+        // Reset the rolls for the next round
+        roll1 = null;
+        roll2 = null;
     }
 }
 
 // Add event listeners to each dice image
 var diceImages = document.querySelectorAll("img");
 
-diceImages[0].addEventListener("click", function() {
-    // Roll both dice when the first dice is clicked
-    rollDice();
-});
-
-diceImages[1].addEventListener("click", function() {
-    // Roll both dice when the second dice is clicked
-    rollDice();
-});
-
-
+diceImages[0].addEventListener("click", rollFirstDice);
+diceImages[1].addEventListener("click", rollSecondDice);
