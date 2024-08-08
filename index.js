ec78ba483017409dde2b339c655648ecd4172a10
MsKavitha12 <143311860+MsKavitha12@users.noreply.github.com>
2024-08-08 18:26:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-var randeomnumber1=Math.floor(Math.random()*6)+1;
+function rollDice() {
+    var randomnumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomdiceimage1 = "dice" + randomnumber1 + ".png";
+    var randomimagesource1 = "images/" + randomdiceimage1;
+    var img1 = document.querySelectorAll("img")[0];
+    img1.setAttribute("src", randomimagesource1);
 
-var randomdiceeimage = "dice"+randeomnumber1 +".png";
+    var randomnumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomdiceimage2 = "dice" + randomnumber2 + ".png";
+    var randomimagesource2 = "images/" + randomdiceimage2;
+    var img2 = document.querySelectorAll("img")[1];
+    img2.setAttribute("src", randomimagesource2);
 
-var randomimagesource = "images/"+randomdiceeimage;
-
-var img1 = document.querySelectorAll("img")[0];
-
-img1.setAttribute("src",randomimagesource);
-
-var randeomnumber2=Math.floor(Math.random()*6)+1;
-
-var randomdiceeimage = "dice"+randeomnumber2 +".png";
-
-var randomimagesource = "images/"+randomdiceeimage;
-
-var img2 = document.querySelectorAll("img")[1];
-
-img2.setAttribute("src",randomimagesource);
-
-if(randeomnumber1 === randeomnumber2){
-    document.querySelector("h1").innerHTML="Draw";
+    if (randomnumber1 === randomnumber2) {
+        document.querySelector("h1").innerHTML = "Draw";
+    } else if (randomnumber1 > randomnumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 wins!";
+    } else {
+        document.querySelector("h1").innerHTML = "Player 2 wins!";
+    }
 }
-else if(randeomnumber1 > randeomnumber2){
-    document.querySelector("h1").innerHTML="Player 1 wins!";
-}
-else{
-    document.querySelector("h1").innerHTML="Player 2 wins!";
-}
+
+// Add event listeners to the dice images
+var diceImages = document.querySelectorAll("img");
+diceImages.forEach(function(dice) {
+    dice.addEventListener("click", rollDice);
+});
+
